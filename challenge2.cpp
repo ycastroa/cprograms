0c01865784d79f3abdaa07ec7df9d30cc5e59a23
ycastroa <mr.castro.y@gmail.com>
2019-03-15 21:57:47 -0400

    second challenge

---
+++ challenge2.cpp
@@ -0,0 +1,11 @@
+
+//hello world
+/*by Yamil  castro*/
+#include <iostream>
+using namespace std;
+int main(){     
+        string name;
+        cin >> name; // ask for users input
+        cout <<name<<"hello world"<<endl;
+        return 0;
+}
